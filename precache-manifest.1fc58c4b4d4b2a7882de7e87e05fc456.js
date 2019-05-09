3d96aa69689caf9f177ef367d831e36abcac59a2
Brett H <Horst.Brett@gmail.com>
2019-05-09 15:33:52 -0600

    test

---
+++ precache-manifest.1fc58c4b4d4b2a7882de7e87e05fc456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f039a5f19cb03dec54682f622b32d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b797e6446a8610654ce",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9fef2c0ad10754186438",
+    "url": "/static/css/main.66e175f8.chunk.css"
+  },
+  {
+    "revision": "3b797e6446a8610654ce",
+    "url": "/static/js/2.f27338f3.chunk.js"
+  },
+  {
+    "revision": "9fef2c0ad10754186438",
+    "url": "/static/js/main.aeacd822.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
