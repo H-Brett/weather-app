cdbc79af58da80d20966fa96ecb2252bc3643486
Brett H <Horst.Brett@gmail.com>
2019-05-13 13:42:20 -0600

    working!

--- src/App.js
+++ src/App.js
@@ -1,49 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Sun from './icons/Sun.png';
-import Cloudy from './icons/Cloudy.png'; 
-import PartlyCloudy from './icons/PartlyCloudy.png';
-import Rain from './icons/Rain.png';
-import Snow from './icons/Snow.png';
-import Storm from './icons/Storm.png';
-import Windy from './icons/Windy.png';
 import SearchBar from './SearchBar.js';
 import CardList from './CardList.js'
 
-const APIKEY = '2e14c8c248ffe44081b033c01ff6eb2b'; 
-
-let fakeData = [
-  {
-    day: 'MON', 
-    icon: Sun, 
-    high: 68, 
-    low: 34,
-  },
-  {
-    day: 'TUE', 
-    icon: Sun, 
-    high: 75, 
-    low: 40,
-  },
-  {
-    day: 'WED', 
-    icon: PartlyCloudy, 
-    high: 55, 
-    low: 32,
-  },
-  {
-    day: 'THU', 
-    icon: Storm, 
-    high: 50, 
-    low: 30,
-  },
-  {
-    day: 'FRI', 
-    icon: Rain, 
-    high: 48, 
-    low: 34,
-  },
-];
+const APIKEY = ''; 
+// const ZIPCODEKEY = ''; 
+const CLIENTSIDEKEY = '';
 
 class App extends Component {
   constructor() {
@@ -51,28 +13,36 @@ class App extends Component {
 
     this.state = {
       data: [], 
+      fetch: {}, 
+      city: '', 
+      stateAbbr: '',  
     }
-  }
+  };
 
-
-  getData = async (zipCode) => {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&mode=json&units=imperial&appid=${APIKEY}`) 
+  getWeatherData = async (city, state) => {
+    let response = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&country=US&state=${state}&units=i&days=5&key=${APIKEY}`);
     let json = await response.json(); 
     let data = await json; 
     this.setState({fetch: data}) 
-    // let filteredData = data.filter((obj, i) => {
-    //        return i === 0 || i == 39 || obj.dt_txt.includes('06:00:00') || obj.dt_txt.includes('15:00:00')
-    //   }); 
   }    
 
+  getCityData = async (zipCode) => {
+    let response = await fetch(`http://www.zipcodeapi.com/rest/${CLIENTSIDEKEY}/info.json/${zipCode}/degrees`);
+    let json = await response.json(); 
+    let data = await json; 
+    this.setState({
+      city: data.city, 
+      stateAbbr: data.state, 
+    })
+  }
+
   componentDidUpdate(prevProps, prevState) {
-
-    let { fetch } = this.state; 
-    let cityName = fetch.city.name;
+    if(this.state.city !== prevState.city){ // if city received trigger weather api call 
+      this.getWeatherData(this.state.city, this.state.stateAbbr)
+    }
     
-
-    if(this.state.fetch !== prevState.fetch) {
-      let list = [...this.state.data, <CardList key={cityName} data={fakeData} realData={fetch} />]
+    if(this.state.fetch !== prevState.fetch) { // if new fetch, create new cardlist
+      let list = [...this.state.data, <CardList key={this.state.city} fetch={this.state.fetch} />]
       this.setState({data: list})
     }
   }
@@ -80,8 +50,8 @@ class App extends Component {
   onSearchChange = (event) => {
     let { value } = event.target; 
     if(value.length === 5 && new RegExp(/[0-9]/).test(value)) { // tests that user input is 5 digits only
-      this.getData(value);
-      event.target.value = ''
+      this.getCityData(value);
+      value = ''
     }
   }
 
@@ -95,7 +65,7 @@ class App extends Component {
         }    
       </div>
     );
-}
+  }
 }
 
 export default App;
--- src/CardList.js
+++ src/CardList.js
@@ -1,22 +1,85 @@
 import React from 'react';
 import Card from './Card';
+import Sun from './icons/Sun.png';
+import Cloudy from './icons/Cloudy.png'; 
+import PartlyCloudy from './icons/PartlyCloudy.png';
+import Rain from './icons/Rain.png';
+import Snow from './icons/Snow.png';
+import Storm from './icons/Storm.png';
+import Windy from './icons/Windy.png';
+import Fog from './icons/Fog.png';
 
-const CardList = ({ data, realData }) => {
-	console.log(realData);
-	let cards = data.map((x, i) => {
+
+const CardList = ({ fetch }) => {
+
+
+	let processedData = fetch.data.map((day, i) => {
+		let returnObj = {
+			high: Math.round(day.max_temp),
+			low: Math.round(day.min_temp), 
+		}; 
+		let icon = day.weather.code; 
+		let date = new Date().getDay() + i; 
+		
+		if(icon  <300) {
+			returnObj.icon = Storm; 
+		} else if ( icon >= 300 && icon < 600 ) {
+			returnObj.icon = Rain; 
+		} else if ( icon >= 600 && icon < 700 ) {
+			returnObj.icon = Snow; 
+		} else if ( icon >= 700 && icon < 800 ) {
+			returnObj.icon = Fog;
+		} else if ( icon === 801 || icon === 802 ) {
+			returnObj.icon = PartlyCloudy; 
+		} else if ( icon === 803 || icon === 804 ) {
+			returnObj.icon = Cloudy; 
+		} else {
+			returnObj.icon = Sun; 
+		}
+
+		switch( date ) {
+			case 0: 
+				returnObj.day = 'SUN'
+				break; 
+			case 1:
+				returnObj.day = 'MON'
+				break;
+			case 2:
+				returnObj.day = 'TUE'
+				break;
+			case 3: 
+				returnObj.day = 'WED'
+				break;
+			case 4: 
+				returnObj.day = 'THU'
+				break;
+			case 5: 
+				returnObj.day = 'FRI'
+				break;
+			case 6: 
+				returnObj.day = 'SAT'
+				break;
+			default: 
+				break; 
+		}
+
+		return returnObj; 
+	});
+
+	let cards = processedData.map((obj, i) => {
 		return (
 			<Card 
 				key={i}
-				day={x.day}
-				icon={x.icon}
-				high={x.high}
-				low={x.low} />
+				day={obj.day}
+				icon={obj.icon}
+				high={obj.high}
+				low={obj.low} />
 		)
 	})
 
 	return (
 		<div className='mt4 dib ba pa3'>
-			<h3 className='tl mb0'>{realData.city.name}</h3>
+			<h3 className='tl mb0'>{fetch.city_name}</h3>
 			{cards}
 		</div>
 
