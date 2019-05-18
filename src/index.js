d305de2f71efd2aa5522aaf5ae69a1e215bf8597
Brett H <Horst.Brett@gmail.com>
2019-05-18 15:54:18 -0600

    test deploy to github pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import Tachyons from 'tachyons';
+import * as serviceWorker from './serviceWorker'; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register(); 
 
